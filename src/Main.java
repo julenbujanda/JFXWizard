db1f9b86eacc6ba044e82a4d8698ee45facc0631
julenb <21701719@live.uem.es>
2018-11-14 19:21:39 +0100

    Main

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/Wizard.fxml"));
+        primaryStage.setTitle("Sign Up");
+        primaryStage.getIcons().add(new Image("img/keyboard.png"));
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+}
