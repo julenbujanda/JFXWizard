201fc1b2456637bb879b0be046ba22a26d9a8756
julenb <21701719@live.uem.es>
2018-11-14 20:04:14 +0100

    Window borders and drag

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double xOffset;
+    private double yOffset;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,10 +20,26 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("view/Wizard.fxml"));
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Sign Up");
         primaryStage.getIcons().add(new Image("img/keyboard.png"));
+        primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, 480, 600));
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Rectangle rect = new Rectangle(480, 600);
+        rect.setArcHeight(50.0);
+        rect.setArcWidth(50.0);
+        root.setOnMousePressed(event -> {
+            xOffset = primaryStage.getX() - event.getScreenX();
+            yOffset = primaryStage.getY() - event.getScreenY();
+        });
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
+        root.setClip(rect);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
