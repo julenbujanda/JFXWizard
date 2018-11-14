b112a224f0fef801465e2449d008933fd17aa227
julenb <21701719@live.uem.es>
2018-11-14 19:39:37 +0100

    Wizard controller

---
+++ src/view/WizardController.java
@@ -0,0 +1,19 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class WizardController {
+
+    @FXML
+    private AnchorPane pane1, pane2, pane3;
+
+    @FXML
+    private Label countLabel;
+
+    public void initialize() {
+
+    }
+
+}
