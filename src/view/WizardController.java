615df96f61fc34a512ba7545a980b3e572b30859
julenb <21701719@live.uem.es>
2018-11-14 19:51:02 +0100

    Animations

--- src/view/WizardController.java
+++ src/view/WizardController.java
@@ -1,11 +1,17 @@
 package view;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class WizardController {
 
+    private final int PANE_WIDTH = 600;
+    private final double TRANSITION_DURATION = 0.5;
+    private int showSlide;
     @FXML
     private AnchorPane pane1, pane2, pane3;
 
@@ -13,7 +19,50 @@ public class WizardController {
     private Label countLabel;
 
     public void initialize() {
+        showSlide = 0;
+    }
 
+    public void translateAnimation(Node node, double byX) {
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(TRANSITION_DURATION), node);
+        translateTransition.setByX(byX);
+        translateTransition.play();
+    }
+
+    @FXML
+    public void nextAction() {
+        switch (showSlide) {
+            case 0:
+                translateAnimation(pane2, -PANE_WIDTH);
+                showSlide++;
+                countLabel.setText("2/3");
+                break;
+            case 1:
+                translateAnimation(pane3, -PANE_WIDTH);
+                showSlide++;
+                countLabel.setText("3/3");
+                break;
+            default:
+                System.out.println("No more slides.");
+        }
+    }
+
+    @FXML
+    public void backAction() {
+        switch (showSlide) {
+            case 0:
+                System.out.println("No more slides.");
+                break;
+            case 1:
+                translateAnimation(pane1, PANE_WIDTH);
+                showSlide--;
+                countLabel.setText("1/3");
+                break;
+            case 2:
+                translateAnimation(pane2, PANE_WIDTH);
+                showSlide--;
+                countLabel.setText("2/3");
+                break;
+        }
     }
 
 }
